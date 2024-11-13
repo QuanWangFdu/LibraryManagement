eb11bf5d3337d7c004d9ecf8c18472f782629be8
QuanWangFdu <q.wang1@student.fdu.edu>
2024-11-13 14:31:58 -0800

    Initial commit

--- obj/Debug/net8.0/LibraryManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryManagement.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c74582e9bb18df5d163cb94c74b8b744dd82862")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
